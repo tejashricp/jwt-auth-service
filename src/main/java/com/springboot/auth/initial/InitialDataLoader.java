93471f8f263e6d4355664ae9d7fb5f192f04fb7f
Tejashri Pathrikar <tejashripathrikar@Tejashris-MacBook-Pro.local>
2019-05-14 15:36:13 +0530

    Adding initial data loader in order to add initial data of users, roles and privs in db

---
+++ src/main/java/com/springboot/auth/initial/InitialDataLoader.java
@@ -0,0 +1,86 @@
+package com.springboot.auth.initial;
+
+import com.springboot.auth.entities.Privilege;
+import com.springboot.auth.entities.Role;
+import com.springboot.auth.entities.User;
+import com.springboot.auth.repositories.PrivilegeRepository;
+import com.springboot.auth.repositories.RoleRepository;
+import com.springboot.auth.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class InitialDataLoader {
+
+    boolean alreadySetup = false;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PrivilegeRepository privilegeRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+
+    @EventListener
+    public void onContextRefreshedEvent(ContextRefreshedEvent event){
+        if (alreadySetup)
+            return;
+        Privilege readPrivilege
+                = createPrivilegeIfNotFound("READ_PRIVILEGE");
+        Privilege writePrivilege
+                = createPrivilegeIfNotFound("WRITE_PRIVILEGE");
+
+        List<Privilege> adminPrivileges = Arrays.asList(
+                readPrivilege, writePrivilege);
+        createRoleIfNotFound("ROLE_ADMIN", adminPrivileges);
+        createRoleIfNotFound("ROLE_USER", Arrays.asList(readPrivilege));
+
+        Role adminRole = roleRepository.findByName("ROLE_ADMIN");
+        User user = new User();
+        user.setFirstName("Test");
+        user.setLastName("Test");
+        user.setPassword(passwordEncoder.encode("test"));
+        user.setUsername("test@test.com");
+        user.setRoles(Arrays.asList(adminRole));
+        user.setEnabled(true);
+        userRepository.save(user);
+
+        alreadySetup = true;
+    }
+
+    @Transactional
+    private Privilege createPrivilegeIfNotFound(String name) {
+
+        Privilege privilege = privilegeRepository.findByName(name);
+        if (privilege == null) {
+            privilege = new Privilege(name);
+            privilegeRepository.save(privilege);
+        }
+        return privilege;
+    }
+
+    @Transactional
+    private Role createRoleIfNotFound(
+            String name, Collection<Privilege> privileges) {
+
+        Role role = roleRepository.findByName(name);
+        if (role == null) {
+            role = new Role(name);
+            role.setPrivileges(privileges);
+            roleRepository.save(role);
+        }
+        return role;
+    }
+}
