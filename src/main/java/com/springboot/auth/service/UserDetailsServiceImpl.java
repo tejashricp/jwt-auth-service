bcb41b5d4eaebb49df3bf1ac05c6e388e8c2a1ee
Tejashri Pathrikar <tejashripathrikar@Tejashris-MacBook-Pro.local>
2019-05-14 16:03:21 +0530

    adding transactional annotation to fix lazyInitializationException

--- src/main/java/com/springboot/auth/service/UserDetailsServiceImpl.java
+++ src/main/java/com/springboot/auth/service/UserDetailsServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,6 +22,7 @@ import java.util.Collection;
 import java.util.List;
 
 @Service
+@Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
